a88e41e87837299d58a4d466fcbe29d75949d509
Harsh Kumar Rai <hkrai1372@gmail.com>
2026-01-28 14:11:03 +0530

    fix: revert on empty or out-of-bounds subarray access

--- src/Maelstrom.sol
+++ src/Maelstrom.sol
@@ -6,11 +6,12 @@ import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import {LiquidityPoolToken} from "./LiquidityPoolToken.sol";
 import {SafeMath} from "node_modules/openzeppelin/contracts/utils/math/SafeMath.sol";
-import {SD59x18,exp} from "prb-math/src/SD59x18.sol";
+import {SD59x18, exp} from "prb-math/src/SD59x18.sol";
 import {ProtocolParameters} from "./ProtocolParameters.sol";
 
 contract Maelstrom {
     using SafeMath for uint256;
+
     struct PoolParams {
         uint256 lastBuyPrice;
         uint256 lastSellPrice;
@@ -26,24 +27,61 @@ contract Maelstrom {
         uint256 decayedBuyVolume;
         uint256 decayedSellVolume;
     }
+
     struct PoolFees {
         uint256 fee;
         uint256 timestamp;
     }
-    event PoolInitialized(address indexed token, uint256 amountToken, uint256 amountEther, uint256 initialPriceBuy, uint256 initialPriceSell);
-    event BuyTrade(address indexed token, address indexed trader, uint256 amountEther, uint256 amountToken, uint256 tradeBuyPrice, uint256 updatedBuyPrice, uint256 sellPrice);
-    event SellTrade(address indexed token, address indexed trader, uint256 amountToken, uint256 amountEther, uint256 tradeSellPrice, uint256 updatedSellPrice, uint256 buyPrice);
-    event SwapTrade(address indexed tokenSold, address indexed tokenBought, address indexed trader, uint256 amountTokenSold, uint256 amountTokenBought, uint256 tradeSellPrice, uint256 updatedSellPrice, uint256 tradeBuyPrice, uint256 updatedBuyPrice);
-    event Deposit(address indexed token, address indexed user, uint256 amountEther, uint256 amountToken, uint256 lpTokensMinted);
-    event Withdraw(address indexed token, address indexed user, uint256 amountEther, uint256 amountToken, uint256 lpTokensBurned);
+    event PoolInitialized(
+        address indexed token,
+        uint256 amountToken,
+        uint256 amountEther,
+        uint256 initialPriceBuy,
+        uint256 initialPriceSell
+    );
+    event BuyTrade(
+        address indexed token,
+        address indexed trader,
+        uint256 amountEther,
+        uint256 amountToken,
+        uint256 tradeBuyPrice,
+        uint256 updatedBuyPrice,
+        uint256 sellPrice
+    );
+    event SellTrade(
+        address indexed token,
+        address indexed trader,
+        uint256 amountToken,
+        uint256 amountEther,
+        uint256 tradeSellPrice,
+        uint256 updatedSellPrice,
+        uint256 buyPrice
+    );
+    event SwapTrade(
+        address indexed tokenSold,
+        address indexed tokenBought,
+        address indexed trader,
+        uint256 amountTokenSold,
+        uint256 amountTokenBought,
+        uint256 tradeSellPrice,
+        uint256 updatedSellPrice,
+        uint256 tradeBuyPrice,
+        uint256 updatedBuyPrice
+    );
+    event Deposit(
+        address indexed token, address indexed user, uint256 amountEther, uint256 amountToken, uint256 lpTokensMinted
+    );
+    event Withdraw(
+        address indexed token, address indexed user, uint256 amountEther, uint256 amountToken, uint256 lpTokensBurned
+    );
     uint256 public auctionResetPercentage = 5;
     uint256 public totalFees = 0;
     address[] public poolList;
     mapping(address => uint256) public totalPoolFees;
     mapping(address => PoolFees[]) public poolFeesEvents;
-    mapping(address => mapping(address => uint256)) public userPoolIndex;  //index+1 is stored
+    mapping(address => mapping(address => uint256)) public userPoolIndex; //index+1 is stored
     mapping(address => address[]) public userPools;
-    mapping(address => LiquidityPoolToken) public poolToken; 
+    mapping(address => LiquidityPoolToken) public poolToken;
     mapping(address => uint256) public ethBalance;
     mapping(address => PoolParams) public pools;
     ProtocolParameters protocolParameters;
@@ -51,7 +89,7 @@ contract Maelstrom {
     constructor(address _protocolParametersAddress) {
         protocolParameters = ProtocolParameters(_protocolParametersAddress);
     }
-    
+
     function sendERC20(address token, address to, uint256 amountToken) internal {
         SafeERC20.safeTransfer(IERC20(token), to, amountToken);
     }
@@ -60,29 +98,19 @@ contract Maelstrom {
         SafeERC20.safeTransferFrom(IERC20(token), from, address(this), amountToken);
     }
 
-function _getSubArray(address[] memory array, uint256 start, uint256 end)
-    internal
-    pure
-    returns (address[] memory)
-{
-    if (array.length == 0) {
-        return new address[](0);
+    function _getSubArray(address[] memory array, uint256 start, uint256 end) internal pure returns (address[] memory) {
+        require(array.length > 0, "Empty array");
+        require(start < array.length, "Start index out of bounds");
+
+        end = end >= array.length ? array.length - 1 : end;
+        require(start <= end, "Invalid start or end index");
+
+        address[] memory subArray = new address[](end - start + 1);
+        for (uint256 i = start; i <= end; i++) {
+            subArray[i - start] = array[i];
+        }
+        return subArray;
     }
-    if (start >= array.length) {
-        return new address[](0);
-    }
-
-    end = end >= array.length ? array.length - 1 : end;
-    require(start <= end, "Invalid start or end index");
-
-    address[] memory subArray = new address[](end - start + 1);
-    for (uint256 i = start; i <= end; i++) {
-        subArray[i - start] = array[i];
-    }
-    return subArray;
-}
-
-
 
     function getPoolList(uint256 start, uint256 end) external view returns (address[] memory) {
         return _getSubArray(poolList, start, end);
@@ -105,7 +133,7 @@ function _getSubArray(address[] memory array, uint256 start, uint256 end)
 
     function _addTokenToUserPools(address user, address token) internal {
         userPools[user].push(token);
-        userPoolIndex[user][token] = userPools[user].length; 
+        userPoolIndex[user][token] = userPools[user].length;
     }
 
     function getTotalPools() external view returns (uint256) {
@@ -120,13 +148,18 @@ function _getSubArray(address[] memory array, uint256 start, uint256 end)
         return poolFeesEvents[token].length;
     }
 
-    function calculateFinalPrice(uint256 decayedSellVolume, uint256 sellPrice, uint256 decayedBuyVolume, uint256 buyPrice) internal pure returns (uint256){
-        if(decayedSellVolume + decayedBuyVolume == 0) return (sellPrice + buyPrice) / 2;
+    function calculateFinalPrice(
+        uint256 decayedSellVolume,
+        uint256 sellPrice,
+        uint256 decayedBuyVolume,
+        uint256 buyPrice
+    ) internal pure returns (uint256) {
+        if (decayedSellVolume + decayedBuyVolume == 0) return (sellPrice + buyPrice) / 2;
         return (decayedSellVolume * sellPrice + decayedBuyVolume * buyPrice) / (decayedSellVolume + decayedBuyVolume);
     }
 
-    function _getDecayValue(uint256 initialVolume, int256 timeElapsed) internal pure returns (uint256){
-        int256 decayedAmount = SD59x18.unwrap(SD59x18.wrap((int256)(initialVolume)) * exp(SD59x18.wrap(-timeElapsed)));  
+    function _getDecayValue(uint256 initialVolume, int256 timeElapsed) internal pure returns (uint256) {
+        int256 decayedAmount = SD59x18.unwrap(SD59x18.wrap((int256)(initialVolume)) * exp(SD59x18.wrap(-timeElapsed)));
         return (uint256)(decayedAmount);
     }
 
@@ -137,8 +170,8 @@ function _getSubArray(address[] memory array, uint256 start, uint256 end)
         poolFeesEvents[token].push(newFee);
         uint256 stableFees = totalFee * protocolParameters.fee() / 10000;
         address feeRecipient = protocolParameters.treasury();
-        (bool success, ) = feeRecipient.call{value: stableFees}(''); 
-        require(success, 'Transfer failed');
+        (bool success,) = feeRecipient.call{value: stableFees}("");
+        require(success, "Transfer failed");
     }
 
     function updatePriceSellParams(address token, uint256 amountToken, uint256 newPrice) internal {
@@ -149,13 +182,16 @@ function _getSubArray(address[] memory array, uint256 start, uint256 end)
         uint256 newDecayedSellVolume = decayedSellVolume + amountToken;
         uint256 newInitialSellPrice = (pool.initialSellPrice * (100 - auctionResetPercentage)) / 100;
         pool.lastSellPrice = newPrice;
-        pool.initialSellPrice = newInitialSellPrice; 
+        pool.initialSellPrice = newInitialSellPrice;
         pool.initialBuyPrice = priceBuy(token);
         pool.decayedSellVolume = newDecayedSellVolume;
         pool.decayedBuyVolume = decayedBuyVolume;
-        pool.finalBuyPrice = calculateFinalPrice(newDecayedSellVolume, newInitialSellPrice, decayedBuyVolume, pool.initialBuyPrice);
+        pool.finalBuyPrice =
+            calculateFinalPrice(newDecayedSellVolume, newInitialSellPrice, decayedBuyVolume, pool.initialBuyPrice);
         pool.finalSellPrice = pool.finalBuyPrice;
-        pool.decayedSellTime = (((block.timestamp - pool.lastSellTimestamp) * amountToken) + (pool.decayedSellTime * decayedSellVolume)) / (amountToken + decayedSellVolume);
+        pool.decayedSellTime =
+            (((block.timestamp - pool.lastSellTimestamp) * amountToken) + (pool.decayedSellTime * decayedSellVolume))
+                / (amountToken + decayedSellVolume);
         pool.lastSellTimestamp = block.timestamp;
         pool.lastExchangeTimestamp = block.timestamp;
     }
@@ -172,9 +208,12 @@ function _getSubArray(address[] memory array, uint256 start, uint256 end)
         pool.initialSellPrice = priceSell(token);
         pool.decayedBuyVolume = newDecayedBuyVolume;
         pool.decayedSellVolume = decayedSellVolume;
-        pool.finalBuyPrice = calculateFinalPrice(decayedSellVolume, pool.initialSellPrice, newDecayedBuyVolume, newInitialBuyPrice);
+        pool.finalBuyPrice =
+            calculateFinalPrice(decayedSellVolume, pool.initialSellPrice, newDecayedBuyVolume, newInitialBuyPrice);
         pool.finalSellPrice = pool.finalBuyPrice;
-        pool.decayedBuyTime = (((block.timestamp - pool.lastBuyTimestamp) * amountToken) + (pool.decayedBuyTime * decayedBuyVolume)) / (amountToken + decayedBuyVolume);
+        pool.decayedBuyTime =
+            (((block.timestamp - pool.lastBuyTimestamp) * amountToken) + (pool.decayedBuyTime * decayedBuyVolume))
+                / (amountToken + decayedBuyVolume);
         pool.lastBuyTimestamp = block.timestamp;
         pool.lastExchangeTimestamp = block.timestamp;
     }
@@ -194,33 +233,39 @@ function _getSubArray(address[] memory array, uint256 start, uint256 end)
     function _preBuy(address token, uint256 amountEther) internal returns (uint256, uint256) {
         ethBalance[token] += amountEther;
         uint256 buyPrice = priceBuy(token);
-        uint256 amountToken = (amountEther * 1e18 ) / buyPrice;
-        require((ERC20(token).balanceOf(address(this)) * 10) / 100 >= amountToken, "Not more than 10% of tokens in pool can be used for swap");
+        uint256 amountToken = (amountEther * 1e18) / buyPrice;
+        require(
+            (ERC20(token).balanceOf(address(this)) * 10) / 100 >= amountToken,
+            "Not more than 10% of tokens in pool can be used for swap"
+        );
         uint256 totalFee = ((buyPrice - pools[token].finalBuyPrice) * amountToken) / 1e18;
         if (totalFee != 0) processProtocolFees(token, totalFee);
         updatePriceBuyParams(token, amountToken, buyPrice);
         return (amountToken, buyPrice);
     }
 
-    function priceBuy(address token) public view returns (uint256){
+    function priceBuy(address token) public view returns (uint256) {
         PoolParams memory pool = pools[token];
         uint256 initialBuyPrice = pool.initialBuyPrice;
         uint256 finalBuyPrice = pool.finalBuyPrice;
         uint256 timeElapsed = block.timestamp - pool.lastExchangeTimestamp;
-        if(timeElapsed >= pool.decayedBuyTime) return finalBuyPrice; 
-        return initialBuyPrice - (((initialBuyPrice - finalBuyPrice) * timeElapsed) / (pool.decayedBuyTime)); 
+        if (timeElapsed >= pool.decayedBuyTime) return finalBuyPrice;
+        return initialBuyPrice - (((initialBuyPrice - finalBuyPrice) * timeElapsed) / (pool.decayedBuyTime));
     }
 
-    function priceSell(address token) public view returns(uint256){
+    function priceSell(address token) public view returns (uint256) {
         PoolParams memory pool = pools[token];
         uint256 initialSellPrice = pool.initialSellPrice;
         uint256 finalSellPrice = pool.finalSellPrice;
         uint256 timeElapsed = block.timestamp - pool.lastExchangeTimestamp;
-        if(timeElapsed >= pool.decayedSellTime) return finalSellPrice;
+        if (timeElapsed >= pool.decayedSellTime) return finalSellPrice;
         return initialSellPrice + (((finalSellPrice - initialSellPrice) * timeElapsed) / (pool.decayedSellTime));
     }
 
-    function initializePool(address token, uint256 amountToken, uint256 initialPriceBuy, uint256 initialPriceSell) public payable {
+    function initializePool(address token, uint256 amountToken, uint256 initialPriceBuy, uint256 initialPriceSell)
+        public
+        payable
+    {
         require(msg.value > 0, "Initial liquidity required");
         require(amountToken > 0, "Initial token liquidity required");
         require(initialPriceBuy > 0 && initialPriceSell > 0, "Initial prices must be > 0");
@@ -241,7 +286,7 @@ function _getSubArray(address[] memory array, uint256 start, uint256 end)
             initialBuyPrice: initialPriceBuy,
             lastBuyTimestamp: block.timestamp,
             lastSellTimestamp: block.timestamp,
-            decayedBuyTime: 0, 
+            decayedBuyTime: 0,
             decayedSellTime: 0,
             decayedBuyVolume: 0,
             decayedSellVolume: 0
@@ -271,7 +316,7 @@ function _getSubArray(address[] memory array, uint256 start, uint256 end)
     }
 
     function buy(address token, uint256 minimumAmountToken) public payable {
-        (uint256 amountToken,uint256 buyPrice) = _preBuy(token, msg.value);
+        (uint256 amountToken, uint256 buyPrice) = _preBuy(token, msg.value);
         require(minimumAmountToken <= amountToken, "Insufficient output amount");
         sendERC20(token, msg.sender, amountToken);
         emit BuyTrade(token, msg.sender, msg.value, amountToken, buyPrice, priceBuy(token), priceSell(token));
@@ -281,14 +326,14 @@ function _getSubArray(address[] memory array, uint256 start, uint256 end)
         receiveERC20(token, msg.sender, amount);
         (uint256 amountEther, uint256 sellPrice) = _postSell(token, amount);
         require(minimumAmountEther < amountEther, "Insufficient output amount");
-        (bool success, ) = msg.sender.call{value: amountEther}(''); 
-        require(success, 'Transfer failed');
+        (bool success,) = msg.sender.call{value: amountEther}("");
+        require(success, "Transfer failed");
         emit SellTrade(token, msg.sender, amount, amountEther, sellPrice, priceSell(token), priceBuy(token));
     }
 
     function deposit(address token) external payable {
         require(msg.value > 0, "Must send ETH to deposit");
-        if(userPoolIndex[msg.sender][token] == 0) _addTokenToUserPools(msg.sender, token);
+        if (userPoolIndex[msg.sender][token] == 0) _addTokenToUserPools(msg.sender, token);
         uint256 ethBalanceBefore = ethBalance[token];
         uint256 amountToken = msg.value * tokenPerETHRatio(token);
         ethBalance[token] += msg.value;
@@ -312,18 +357,18 @@ function _getSubArray(address[] memory array, uint256 start, uint256 end)
         uint256 amountEther = (rETH * amountPoolToken) / ts;
         uint256 amountEtherAfterFees = (amountEther * 995) / 1000;
         ethBalance[token] -= amountEtherAfterFees;
-        (bool success, ) = msg.sender.call{value: amountEtherAfterFees}('');
-        if(pt.balanceOf(msg.sender) == 0){
+        (bool success,) = msg.sender.call{value: amountEtherAfterFees}("");
+        if (pt.balanceOf(msg.sender) == 0) {
             //Token is removed using swap and pop method(swap it with last element and pop it O(1))
             address[] storage currentPools = userPools[msg.sender];
             mapping(address => uint256) storage poolIndex = userPoolIndex[msg.sender];
             uint256 index = userPoolIndex[msg.sender][token] - 1;
             poolIndex[token] = 0;
             uint256 lastIndex = userPools[msg.sender].length - 1;
-            if(index != 0){
+            if (index != 0) {
                 address lastToken = currentPools[lastIndex];
                 currentPools[index] = lastToken;
-                poolIndex[lastToken] = index + 1; 
+                poolIndex[lastToken] = index + 1;
             }
             currentPools.pop();
         }
@@ -332,11 +377,21 @@ function _getSubArray(address[] memory array, uint256 start, uint256 end)
     }
 
     function swap(address tokenSell, address tokenBuy, uint256 amountToSell, uint256 minimumAmountToken) external {
-        (uint256 amountEther, uint256 sellPrice)  = _postSell(tokenSell, amountToSell);
+        (uint256 amountEther, uint256 sellPrice) = _postSell(tokenSell, amountToSell);
         (uint256 amountToken, uint256 buyPrice) = _preBuy(tokenBuy, amountEther);
         require(amountToken >= minimumAmountToken, "Insufficient output amount");
         receiveERC20(tokenSell, msg.sender, amountToSell);
         sendERC20(tokenBuy, msg.sender, amountToken);
-        emit SwapTrade(tokenSell, tokenBuy, msg.sender, amountToSell, amountToken, sellPrice, priceSell(tokenSell), buyPrice, priceBuy(tokenBuy));
+        emit SwapTrade(
+            tokenSell,
+            tokenBuy,
+            msg.sender,
+            amountToSell,
+            amountToken,
+            sellPrice,
+            priceSell(tokenSell),
+            buyPrice,
+            priceBuy(tokenBuy)
+        );
     }
 }
